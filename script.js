3dd9ad719a85d941b9e40eaaa137e9616914331c
GenoCasceillo <genocasceillo@kings.edu>
2022-09-12 16:12:15 -0400

    v17

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const hamburger = document.querySelector(".hamburger"); // the hamburger button
 const navMenu = document.querySelector(".nav-menu");    // the navigation menu
 const navLink = document.querySelectorAll(".nav-link"); // array of all of the links in the nav menu
 
-hamburger.addEventListener('click', function() );
+hamburger.addEventListener('click', function()
+{
   hamburger.classList.toggle('isactive');
   menu.classList.toggle('active');
+});
