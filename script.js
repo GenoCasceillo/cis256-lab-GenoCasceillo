a3e392f869e7058b801cf3b031c476792f3a876b
GenoCasceillo <genocasceillo@kings.edu>
2022-09-12 15:53:39 -0400

    V14

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const hamburger = document.querySelector(".hamburger"); // the hamburger button
+const navMenu = document.querySelector(".nav-menu");    // the navigation menu
+const navLink = document.querySelectorAll(".nav-link"); // array of all of the links in the nav menu
+
+// Add an event listener so the hamburger calls openMenu when it is clicked
+
+// Your code here!
+element.addEventListener(".hamburger");
+
+hamburger.document('click', function()
+{
+hamburger.document.toggle('isactive');
+menu.document.toggle('active');
+});
