7c9aa207719690b1c800f10a13f9153826c6ec85
GenoCasceillo <genocasceillo@kings.edu>
2022-09-19 14:07:18 -0400

    Fk it

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 const hamburger = document.querySelector(".hamburger"); // the hamburger button
 const navMenu = document.querySelector(".nav-menu");    // the navigation menu
-const navLink = document.querySelectorAll(".nav-link"); // array of all of the links in the nav menu
+const navLink = document.querySelectorAll(".nav-link");
+const body = document.getElementsByTagName("body")[0];
+const darkmode = document.querySelector("#dark");
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
+const bar = document.querySelectorAll(".bar");
+ // array of all of the links in the nav menu
 
-// Add an event listener so the hamburger calls openMenu when it is clicked
-
+hamburger.addEventListener("click", toggleActive);
+darkmode.addEventListener("click", toggleDark);
 // Your code here!
 
 
@@ -15,11 +21,14 @@ const navLink = document.querySelectorAll(".nav-link"); // array of all of the l
 // elements so that the hamburger will switch to an X and the navMenu will
 // slide out, or the hamburger will go back to a burger and the menu will slide in
 function toggleActive() {
-    // Your code here!
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
 
 
-
+for(var i = 0; i < navLink.length; i++){
+  navLink[i].addEventListener("click", closeMenu);
+}
 // Add an event listener FOR EACH navigation link so the menu will close
 // when it is clicked
 
@@ -32,5 +41,16 @@ function toggleActive() {
 // this needs to remove the class "active" from both hamburger and navMenu
 function closeMenu() {
 
-  // Your code here!
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+function toggleDark(){
+  body.classList.toggle("darkmode");
+  header.classList.toggle("darkmode");
+  footer.classList.toggle("darkmode");
+  for(var i = 0; i < bar.length; i++){
+    bar[i].classList.toggle("darkmode");
+  }
+
 }
