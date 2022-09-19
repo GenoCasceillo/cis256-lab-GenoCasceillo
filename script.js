02072f0685d1eb35b763a434ad6d89295057af2d
GenoCasceillo <genocasceillo@kings.edu>
2022-09-19 12:59:18 -0400

    h

--- script.js
+++ script.js
@@ -1,123 +1,36 @@
-.bright {
-  color: orange;
-  font-family: Avenir;
-}
-body {
-  background-color: red;
-    margin-left: 400px;
-    margin-right: 400px;
-    text-align: center;
-    border: 3px solid gold;
-}
-h1 {
-    text-align: center;
-  color: gold;
-}
-p {
-  color: white;
-    text-align: center;
-}
-table {
-      text-align: center;
-      border: 3px solid gold;
-      margin-left: auto;
-      margin-right: auto;
-}
-ul {
-    text-align: center;
-    list-style-position: inside;
-}
-body {
-  position: relative
-}
-footer{
-    text-align: center;
-    color: gold;
-}
-div#holiday-ornament {
-  background: url("https://m.media-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg:*") no-repeat;
-  width: 800px; /*set the width and height to the width and height of the image */
-  height: 800px;
-  text-indent: -9999px; /* Move the text inside of the div off screen. Still readable for text-readers for the blind */
-  position: absolute;
-  left: -113%;
-  top: -25px;
-}
-div#trans2 {
-  background: url("https://m.media-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg:*") no-repeat;
-  width: 400px;
-  height: 800px;
-  text-indent: -9999px; /* Move the text inside of the div off screen. Still readable for text-readers for the blind */
-  position: absolute;
-  left: 101%;
-  top: -25px;
-}
-nav li 	 {list-style: none;} /* No bullets */
 
-nav a 	 {text-decoration: none;} /* No underline on links in navbar */
+const hamburger = document.querySelector(".hamburger"); // the hamburger button
+const navMenu = document.querySelector(".nav-menu");    // the navigation menu
+const navLink = document.querySelectorAll(".nav-link"); // array of all of the links in the nav menu
 
-/* flex layout lets the navbar be responsive to screen size */
-.navbar   { display: flex; align-items: center; padding: 1rem 1.5rem;}
+// Add an event listener so the hamburger calls openMenu when it is clicked
 
-/* Sets margins around nav items */
-.nav-item { margin-left: 5rem; margin-right: 2rem}
+// Your code here!
 
-/* Sets the look of the links */
-.nav-link{ font-size: 1.6rem; font-weight: 400; color: #475569;}
 
-/* Changes color of link when hover over it */
-.nav-link:hover { color: #482ff7;}
 
-/* z-index makes sure menu shows up on top of other elements,
-   transition is speed menu items transition to active
-   left: -100% hides the menu off the screen when it's not active */
-.nav-menu {
-    z-index: 3;
-    position: fixed;
-    left: -100%;
-    top: 55%;
-    flex-direction: column;
-    background:rgba(255,255,255,1.0);
-    text-align: center;
-    transition: 0.3s;
+
+// function to open the navigation menu
+// this needs to add/remove the class "active" for both the hamburger and navMenu
+// elements so that the hamburger will switch to an X and the navMenu will
+// slide out, or the hamburger will go back to a burger and the menu will slide in
+function toggleActive() {
+    // Your code here!
 }
 
-/* When menu is active display it on the side by setting its left
-position to 0 (no longer hiding at left position -100%) */
-.nav-menu.active {
-    left: 0;
-}
 
-/* display the hamburger as a clickable button, but make it blend in */
-.hamburger {
-    display: block;
-    cursor: pointer;
-    padding: 0;
-    border: none;
-    background: none;
-}
 
-/* draw the hamburger--all spans will be filled with dark background and stacked on top of each other */
-.bar {
-    display: block;
-    width: 25px;
-    height: 3px;
-    margin: 5px auto;
-    transition: all 0.3s ease-in-out;
-    background-color: #101010;
-}
+// Add an event listener FOR EACH navigation link so the menu will close
+// when it is clicked
 
-/* When hamburger is active, hide middle bar */
-.hamburger.active .bar:nth-child(2) {
-    opacity: 0;
-}
+// Your code here!
 
-/* When hamburger is active, rotate top bar 45deg to make one cross of X */
-.hamburger.active .bar:nth-child(1) {
-    transform: translateY(8px) rotate(45deg);
-}
 
-/* When hamburger is active, rotate bottom bar -45deg to make other cross of X */
-.hamburger.active .bar:nth-child(3) {
-    transform: translateY(-8px) rotate(-45deg);
+
+
+// function to close the navigation nav
+// this needs to remove the class "active" from both hamburger and navMenu
+function closeMenu() {
+
+  // Your code here!
 }
