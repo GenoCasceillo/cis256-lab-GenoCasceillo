4e17d8e0fd80363ae1464a31da299f19e4bd0f8c
GenoCasceillo <genocasceillo@kings.edu>
2022-09-29 17:34:09 -0400

    ye

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 // fetch data from a public API
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+function doAPICall() {
+
+  fetch("https://api.kanye.rest")
   .then(response => response.json())
   .then(data => processData(data));
-  # your fetch code here
 
 }
 
@@ -10,19 +11,13 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 // callback function to be used when data is fetched from API
 // parameter data will be a JavaScript object
 function processData(data) {
- var api = document.getElementById("api");
-  # Get the div or other DOM element needed for the data
 
+  var api = document.getElementById("api");
 
-    var cat = data.value;
+  var ye = data.value;
 
-  # Access the necessary fields of the data
-
-
-  # Update the DOM to display the data in some way
-
-      var text = document.createTextNode(cat);
-      var p = document.createElement("p");
-      p.appendChild(text);
-      api.appendChild(p);
+  var text = document.createTextNode(ye);
+  var p = document.createElement("p");
+  p.appendChild(text);
+  api.appendChild(p);
 }
